369293e97ccd68da8f9f2ea9ef9fc42fe88b3c19
Marco Preciado <mac46005@gmail.com>
2022-02-28 16:16:32 -0600

    Adding nuget packages to the wpf project.

--- BudgetApp/BudgetApp_WPF/App.xaml.cs
+++ BudgetApp/BudgetApp_WPF/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,31 @@ namespace BudgetApp_WPF
     /// </summary>
     public partial class App : Application
     {
+
+        IServiceProvider serviceProvider;
+        IConfiguration Configuration { get; set; }
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+
+        private IServiceProvider CreateServiceProvider()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(Configuration);
+
+
+            return services.BuildServiceProvider();
+        }
+
+
+        private IConfiguration BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false);
+            return builder.Build();
+        }
     }
 }
