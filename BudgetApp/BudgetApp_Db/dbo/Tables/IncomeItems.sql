b06cbc4009757c625da07d7ec3428587b44c26d8
Marco Preciado <mac46005@gmail.com>
2022-03-06 11:56:38 -0600

    Tables created in sql project. Revise later. Also revise classes and implement interfaces for the classes

---
+++ BudgetApp/BudgetApp_Db/dbo/Tables/IncomeItems.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[IncomeItems]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [PersonID] INT NOT NULL, 
+    [IncomeCategory_ID] INT NOT NULL, 
+    [Amount] MONEY NOT NULL DEFAULT 0, 
+    [TimeStamp] DATETIME2 NOT NULL, 
+    [IsReOccuring] BIT NOT NULL, 
+    [BudgetFrequency] INT NOT NULL, 
+    [Note] NVARCHAR(50) NULL
+)
