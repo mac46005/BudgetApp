afd0790e1771e461c1b3020d2968a9ef610ff051
Marco Preciado <mac46005@gmail.com>
2022-03-08 05:08:50 -0600

    Adding props for testing views

--- BudgetApp/BudgetApp_WPF/MVVM/ViewModels/MainViewModel.cs
+++ BudgetApp/BudgetApp_WPF/MVVM/ViewModels/MainViewModel.cs
@@ -1,13 +1,62 @@
-﻿using System;
+﻿using BudgetApp_WPF.Core;
+using BudgetApp_WPF.MVVM.ViewModels.Category;
+using BudgetApp_WPF.MVVM.ViewModels.Expense;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BudgetApp_WPF.MVVM.ViewModels
 {
-    internal class MainViewModel<T> : BaseViewModel<IEnumerable<T>>
+    internal class MainViewModel : ObservableObject
     {
 
+        private CategoryDashBoardViewModel _categoryDashBoardViewModel;
+        public CategoryDashBoardViewModel CategoryDashBoardViewModel
+        {
+            get { return _categoryDashBoardViewModel; }
+            set
+            {
+                _categoryDashBoardViewModel = value;
+                OnPropertyChanged("CategoryDashBoardViewModel");
+            }
+        }
+
+
+
+
+        private CurrentMonthViewModel _currentMonethViewModel;
+        public CurrentMonthViewModel CurrentMonthViewModel 
+        {
+            get { return _currentMonethViewModel; }
+            set 
+            {
+                _currentMonethViewModel = value;
+                OnPropertyChanged("CurrentMonthViewModel");
+            } 
+        }
+
+
+
+
+
+        private ExpenseDashBoardViewModel _expenseDashBoardViewModel;
+        public ExpenseDashBoardViewModel ExpenseDashBoardViewModel
+        {
+            get { return _expenseDashBoardViewModel; }
+            set
+            {
+                _expenseDashBoardViewModel = value;
+                OnPropertyChanged("ExpenseDashBoardViewModel");
+            }
+        }
+
+
+
+        public MainViewModel()
+        {
+        }
     }
 }
