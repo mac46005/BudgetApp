5e14f65d0da6748c6dcb4cea83ec8ca6621320b9
Marco Preciado <mac46005@gmail.com>
2022-03-12 18:33:59 -0600

    Update repo

--- BudgetApp_WPF/Core/Commands/SelectMainMenuOptionCommand.cs
+++ BudgetApp_WPF/Core/Commands/SelectMainMenuOptionCommand.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using BudgetApp_WPF.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BudgetApp_WPF.Core.Commands
 {
-    internal class SelectMainMenuOptionCommand
+    internal class SelectMainMenuOptionCommand : ICommand
     {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            MainMenuOptionsEnum mainMenuOptions = (MainMenuOptionsEnum)parameter;
+
+        }
     }
 }
