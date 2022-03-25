5d2364468829ac492af00a17378a91cc52772765
Marco Preciado <mac46005@gmail.com>
2022-03-25 18:32:11 -0500

    For the user item in the ListView implement a way for the user to update or delete the user item. Then replicate it for the other data types

--- BudgetApp_WPF/MVVM/ViewModels/User/UserDashBoardViewModel.cs
+++ BudgetApp_WPF/MVVM/ViewModels/User/UserDashBoardViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace BudgetApp_WPF.MVVM.ViewModels.User
 {
@@ -14,12 +15,21 @@ namespace BudgetApp_WPF.MVVM.ViewModels.User
         public UserDashBoardViewModel(IAPIEndpoint<Budget_ClassLib.Models.User,int> usersDataEndPoint)
         {
             _usersDataEndPoint = usersDataEndPoint;
+
+
+            LoadUserFromAPI();
+
+
+
         }
 
 
+        ObservableCollection<Budget_ClassLib.Models.User> UsersCollection { get; set; }
 
-
-
+        private async void LoadUserFromAPI()
+        {
+            UsersCollection = new ObservableCollection<Budget_ClassLib.Models.User>(await _usersDataEndPoint.GetAsync());
+        }
 
     }
 }
