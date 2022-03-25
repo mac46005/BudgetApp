4c8470a32049b238a0d6852ff8b92426a6052a18
Marco Preciado <mac46005@gmail.com>
2022-03-25 13:01:45 -0500

    Creating an API access class library

---
+++ BudgetDB_APIAccess_ClassLib/API/APIClient.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetDB_APIAccess_ClassLib.API
+{
+    public class APIClient
+    {
+
+        IConfiguration _configuration;
+
+
+
+
+        private HttpClient _apiClient;
+        public HttpClient APIClientObject
+        {
+            get
+            {
+                return _apiClient;
+            }
+        }
+
+
+
+
+        public APIClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+
+
+        private void InitializeClient()
+        {
+            _apiClient = new HttpClient();
+            _apiClient.BaseAddress = new Uri(_configuration.GetConnectionString("BudgetAPI"));
+            _apiClient.DefaultRequestHeaders.Accept.Clear();
+            _apiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+    }
+}
