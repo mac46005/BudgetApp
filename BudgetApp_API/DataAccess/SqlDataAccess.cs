18cbe253cae3a3902386442f00ea473e3d087ed4
Marco Preciado <mac46005@gmail.com>
2022-03-25 01:14:13 -0500

    API dataaccess class

---
+++ BudgetApp_API/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace BudgetApp_API.DataAccess
+{
+    public class SqlDataAccess 
+    {
+        IConfiguration _configuration;
+        public SqlDataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private string GetConnectionString(string connectionName)
+        {
+            return _configuration.GetConnectionString(connectionName);
+        }
+
+
+        public T LoadSingleData<T,U>(string connectionName, string storedProcedure,U parameter)
+        {
+            using (IDbConnection conn = new SqlConnection(GetConnectionString(connectionName)))
+            {
+                var result = conn.Query<T>(storedProcedure, parameter, commandType: CommandType.StoredProcedure);
+                return result.FirstOrDefault();
+            }
+        }
+        public IEnumerable<T> LoadMultiData<T>(string connectionName, string storedProcedure)
+        {
+            using(IDbConnection conn = new SqlConnection(GetConnectionString(connectionName)))
+            {
+                return conn.Query<T>(storedProcedure, commandType: CommandType.StoredProcedure);
+            }
+        }
+        public void ModifyData<T,U>(string connectionName, string storedProcedure, U parameter)
+        {
+            using(IDbConnection conn = new SqlConnection(GetConnectionString(connectionName)))
+            {
+                conn.Query<T>(storedProcedure, parameter, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
