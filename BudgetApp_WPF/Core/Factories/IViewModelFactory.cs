6b39268b736a6c880885bf78ff4c6784add1117e
Marco Preciado <mac46005@gmail.com>
2022-03-26 12:54:26 -0500

    ICreateViewModel

---
+++ BudgetApp_WPF/Core/Factories/IViewModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using BudgetApp_WPF.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp_WPF.Core.Factories
+{
+    internal interface IViewModelFactory<T,U> where T : BaseViewModel<U>
+    {
+        BaseViewModel<U> CreateViewModel();
+    }
+}
